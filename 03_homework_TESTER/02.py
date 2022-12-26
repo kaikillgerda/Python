f613e6e402b323c379264c4509667aa24d9e3f45
kaikillgerda <108028047+kaikillgerda@users.noreply.github.com>
2022-12-26 22:57:18 +0300

    #3 homework tester

---
+++ 03_homework_TESTER/02.py
@@ -0,0 +1,35 @@
+# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# in
+# 4
+
+# out
+# [2, 5, 8, 10]
+# [20, 40]
+
+# in
+# 5
+
+# out
+# [2, 2, 4, 8, 8]
+# [16, 16, 4]
+
+from random import randint
+
+
+number = int(input('Введите размер списка '))
+list = []
+list2 = []
+
+for i in range(number):
+    list.append(randint(1, 9))
+
+for i in range(len(list)):
+    while i < len(list)/2 and number > len(list)/2:
+        number = number - 1
+        a = list[i] * list[number]
+        list2.append(a)
+        i += 1
+
+print(list)
+print(list2)
