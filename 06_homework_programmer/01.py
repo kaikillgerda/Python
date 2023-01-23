728b3c4ede60abc0b200e235b2a74f0753fb5225
kaikillgerda <108028047+kaikillgerda@users.noreply.github.com>
2023-01-23 19:30:08 +0300

    6 homework programmer

--- 06_homework_programmer/01.py
+++ 06_homework_programmer/01.py
@@ -11,4 +11,4 @@ def more_then(num):
     return [my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num - 1]]
 
 
-print(more_then(int(input())))
+print(more_then(int(input('print the number: '))))
--- 06_homework_programmer/02.py
+++ 06_homework_programmer/02.py
@@ -5,4 +5,4 @@ def uniq_list(num):
     return [el for el in range(20, num + 1) if el % 20 == 0 or el % 21 == 0]
 
 
-print(uniq_list(int(input())))
+print(uniq_list(int(input('print the number: '))))
